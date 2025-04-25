eeb7fcf74f790bbdd935ee890185ac5afb251178
Koushikeya <koushikeyajangam@gmail.com>
2025-04-25 16:03:33 +0530

    add register controllers and bug fixes

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.use(express.urlencoded({extended: true, limit: '16kb'}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// router import
+import userRouter from router
+import { router } from "./routes/user.routes.js";
+
+app.use("/users", userRouter )
+
+
 export default app;
